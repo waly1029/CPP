a24981fcfc7350a276b55e8e258e91f5e2472c3f
lz <waly1019@gmail.com>
2023-05-21 15:25:07 +0800

    update

--- CPPSTL/1.STL体系结构基础/main.cpp
+++ CPPSTL/1.STL体系结构基础/main.cpp
@@ -4,3 +4,27 @@
 // 仿函数
 // 适配器
 // 分配器
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int ia[6] = {27, 210, 12, 47, 109, 83};
+
+    vector<int, allocator<int>> vi(ia, ia+6);
+    // allocator
+    // container
+
+    // TODO: 实现bind2nd,not1
+    cout << count_if(vi.begin(), vi.end(), not1(bind2nd(less<int>(), 40))); // >= 40
+    // vi.begin(): iterator
+    // count_if: algorithm
+    // not1: function adapter(negator)
+    // bind2nd: function adapter(binder)
+    // less: function object
+
+    return 0;
+}
